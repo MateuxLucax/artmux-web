c219f943cdee09d561d49298e9436ceb3494d9d6
MateuxLucax <mateuxlucax@gmail.com>
2022-05-17 22:12:07 -0300

    feat: added login screen

--- public/header.php
+++ public/header.php
@@ -3,25 +3,29 @@
 
 <head>
   <meta charset="utf-8">
-  <title>ArTMux | <?= $titulo ?? 'Sem título' ?></title>
+  <title>artmux | <?= $titulo ?? 'Sem título' ?></title>
   <meta name="description" content="Compartilhe sua arte nas redes sociais facilmente">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:title" content="ArTMux">
+  <meta property="og:title" content="artmux">
   <meta property="og:type" content="Open">
   <meta property="og:url" content="artmux.gargantua.one">
   <meta property="og:image" content="static/img/artmux-image.png">
 
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="shortcut icon" href="/static/img/artmux-128x128.png" type="image/x-png" />
   <link rel="shortcut icon" href="/static/img/artmux.svg" type="image/svg+xml" />
 
-  <link rel="stylesheet" href="/static/css/normalize.css">
-  <link rel="stylesheet" href="/static/css/main.css">
+  <link rel="stylesheet" href="/static/css/normalize.css" />
+  <link rel="stylesheet" href="/static/css/main.css" />
 
-  <meta name="theme-color" content="#f5f5f5">
+  <meta name="theme-color" content="#f5f5f5" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Nunito:wght@400;900&display=swap" rel="stylesheet" />
 
   <script src="/static/js/agendar-alerta-swal.js"></script>
   <script src="/static/js/lib.js"></script> <!-- components depends on lib! -->
