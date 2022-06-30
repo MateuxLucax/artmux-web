662609532830a955f4e332adf0983250099ddcd0
MateuxLucax <mateuxlucax@gmail.com>
2022-06-29 23:02:35 -0300

    feat: add tabs on profile page

--- public/perfil/index.php
+++ public/perfil/index.php
@@ -3,44 +3,63 @@ $titulo = 'perfil';
 require_once('../components/head.php');
 require_once('../components/header.php');
 ?>
-<main class="container-fluid d-flex flex-column max-width-480">
-    <section class="my-2 mt-4 mx-auto">
-        <h1 class="text-primary text-center">perfil</h1>
-    </section>
-    <form class="card p-4" method="POST" id="user-info-form">
-        <div class=" form-group my-auto">
-            <label for="username" class="form-label">usuário</label>
-            <input class="form-control" id="username" aria-describedby="zecaurubu" placeholder="zecaurubu" required />
-        </div>
-        <div class="form-group mt-2">
-            <label for="email" class="form-label">email</label>
-            <input type="email" class="form-control" id="email" placeholder="zecaurubu@artmux.dev" required />
-        </div>
-        <div class="d-grid mt-4 mx-auto">
-            <button class="btn btn-primary" id="update-info-btn" type="submit">alterar dados</button>
-        </div>
-    </form>
 
-    <section class="my-2 mx-auto">
-        <h1 class="text-primary text-center">senha</h1>
+<style>
+    .nav-item:not(:last-child) {
+        margin-right: 24px;
+    }
+</style>
+
+<main class="container-fluid d-flex flex-column max-width-480">
+
+    <ul class="nav nav-pills" role="tablist" style="margin-top: 48px;">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="true">Perfil</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="passwords-tab" data-bs-toggle="tab" data-bs-target="#passwords-tab-pane" type="button" role="tab" aria-controls="passwords-tab-pane" aria-selected="false">Senha</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Contas</button>
+        </li>
+    </ul>
+
+    <section class="tab-content" style="margin-top: 24px;">
+        <section class="tab-pane fade show active" id="profile-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+            <form class="card p-4" method="POST" id="user-info-form">
+                <div class=" form-group my-auto">
+                    <label for="username" class="form-label">usuário</label>
+                    <input class="form-control" id="username" aria-describedby="zecaurubu" placeholder="zecaurubu" required />
+                </div>
+                <div class="form-group mt-2">
+                    <label for="email" class="form-label">email</label>
+                    <input type="email" class="form-control" id="email" placeholder="zecaurubu@artmux.dev" required />
+                </div>
+                <div class="d-grid mt-4 mx-auto">
+                    <button class="btn btn-primary" id="update-info-btn" type="submit">alterar dados</button>
+                </div>
+            </form>
+        </section>
+        <section class="tab-pane fade" id="passwords-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+            <form class="card p-4" method="POST" id="user-password-form">
+                <div class="form-group mt-2">
+                    <label for="password" class="form-label">senha atual</label>
+                    <input type="password" class="form-control" id="password" placeholder="********" required />
+                </div>
+                <div class="form-group mt-2">
+                    <label for="newPassword" class="form-label">nova senha</label>
+                    <input type="password" class="form-control" id="newPassword" placeholder="********" required />
+                </div>
+                <div class="form-group mt-2">
+                    <label for="newPasswordConfirmation" class="form-label">confirmação da nova senha</label>
+                    <input type="password" class="form-control" id="newPasswordConfirmation" placeholder="********" required />
+                </div>
+                <div class="d-grid mt-4 mx-auto">
+                    <button class="btn btn-primary" id="update-password-btn" type="submit">alterar senha</button>
+                </div>
+            </form>
+        </section>
     </section>
-    <form class="card p-4" method="POST" id="user-password-form">
-        <div class="form-group mt-2">
-            <label for="password" class="form-label">senha atual</label>
-            <input type="password" class="form-control" id="password" placeholder="********" required />
-        </div>
-        <div class="form-group mt-2">
-            <label for="newPassword" class="form-label">nova senha</label>
-            <input type="password" class="form-control" id="newPassword" placeholder="********" required />
-        </div>
-        <div class="form-group mt-2">
-            <label for="newPasswordConfirmation" class="form-label">confirmação da nova senha</label>
-            <input type="password" class="form-control" id="newPasswordConfirmation" placeholder="********" required />
-        </div>
-        <div class="d-grid mt-4 mx-auto">
-            <button class="btn btn-primary" id="update-password-btn" type="submit">alterar senha</button>
-        </div>
-    </form>
 </main>
 
 <footer class="mx-auto mt-4 mb-2 nunito text-center">
