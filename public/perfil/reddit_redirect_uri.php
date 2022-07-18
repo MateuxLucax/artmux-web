7439da02094b2b8d9b01dafe70e58b559ecb6dc6
MateuxLucax <mateuxlucax@gmail.com>
2022-07-18 19:45:06 -0300

    feat: add loading on reddit_redirect_uri

--- public/perfil/reddit_redirect_uri.php
+++ public/perfil/reddit_redirect_uri.php
@@ -1,10 +1,16 @@
 <?php
-$titulo = 'Acesso ao Reddit';
+$titulo = 'acesso ao Reddit';
 require_once('../components/head.php');
 require_once('../components/header.php');
 ?>
 
 <?php require('../components/scripts.php') ?>
+
+<div class="loading-screen">
+  <div class="spinner-border text-light" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
 
 <script>
   const params = new URLSearchParams(location.search);
@@ -12,19 +18,24 @@ require_once('../components/header.php');
     $message.error('Redirect URI acessada incorretamente', 'Erro do sistema')
             .then(() => { location.assign('/perfil#accounts') })
   } else {
-
     const state = params.get('state');
     const code = params.get('code');
     request.auth.post('reddit/callback', { state, code })
-    .then(() => {
-      $message.successOk('Conta do Reddit vinculada com sucesso')
-              .then(() => { location.assign('/perfil#accounts') })
+    .then((response, json) => {
+      if (response.status === 200) {
+        $message.successOk('Conta do Reddit vinculada com sucesso')
+              .then(() => { location.assign('/perfil#accounts') })  
+      } else {
+        throw { message: "oopsie doopsie" }
+      }
     })
     .catch(err => {
       console.error(err)
       $message.error('Não conseguimos vincular a conta do Reddit')
     })
-
+    .finally(() => {
+      q.hide(q.sel('.loading-screen'));
+    });
   }
 </script>
 
